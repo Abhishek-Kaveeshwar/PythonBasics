ca0af0fd97910f6fc65bbe516ab46173500c54c0
Abhishek-Kaveeshwar <abhi10ak@gmail.com>
2021-03-25 21:34:38 +0530

    index example

--- SumOfFirstFiveNaturalNumbers.py
+++ SumOfFirstFiveNaturalNumbers.py
@@ -2,3 +2,8 @@ summation = 0
 for j in range (1,6): # range(i,j)-> i to j-1
     summation = summation+j
 print(summation)
+
+
+print("********************") # passing index in for loop
+for k in range (1,10,5):
+    print(k)
