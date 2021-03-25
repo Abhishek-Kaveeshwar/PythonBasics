8a75aeb2958610f781498531f4add64e8aa3a7a1
Abhishek-Kaveeshwar <abhi10ak@gmail.com>
2021-03-25 20:22:18 +0530

    adding if else example

---
+++ IfElseCondn.py
@@ -0,0 +1,8 @@
+greeting = "Good Morning"
+
+if greeting == "Good Morning":
+    print("Condition Matches")
+    print("second line")
+else:
+    print("Condition doesn't matches")
+print("completed")
